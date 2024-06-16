117f04a136b90457d07a83ea3daf70aab01564d9
skillitt <markdevore@outlook.com>
2024-06-16 13:44:43 -0400

    fixing

--- sql/fashion_magazines.sql
+++ sql/fashion_magazines.sql
@@ -1,9 +1,5 @@
 SELECT
-<<<<<<< HEAD
     customers.customer_name AS 'Customer',
-=======
-    customers.customer_name AS Customer,
->>>>>>> 57e4150042203ef1ff5333807121c80d13219e16
     PRINTF('$%.2f', SUM(subscriptions.subscription_length * subscriptions.price_per_month)) AS "Amount Due" --had to use internet search here
 FROM 
     orders 
